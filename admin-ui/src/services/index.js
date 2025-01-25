3c6916e28e596476fe610376f64576d88f771d59
Ubuntu <ubuntu@ip-172-31-37-60.ap-south-1.compute.internal>
2025-01-25 11:06:07 +0000

    updated Dockerfiles

--- admin-ui/src/services/index.js
+++ admin-ui/src/services/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { env } from '../env'
 
 const sevice = axios.create({
-    baseURL: `${env.REACT_APP_API_URL}/api`,
+    baseURL: `${env.REACT_APP_API_URL}api`,
 });
 
 export default sevice;
